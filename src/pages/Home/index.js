3a70eff929ab552909413f74b796ec3bc7813b8a
Diego Felipe Florença <diegoflorenca@gmail.com>
2020-07-31 10:33:56 -0300

    Faltando apenas o cadastro de vídeos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
-// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from "../../components/BannerMain";
 import Carousel from "../../components/Carousel";
 import PageDefault from "../../components/PageDefault";
@@ -42,28 +41,6 @@ function Home() {
 
         return <Carousel key={categoria.id} category={categoria} />;
       })}
-
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que"
-      />
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      /> */}
     </PageDefault>
   );
 }
